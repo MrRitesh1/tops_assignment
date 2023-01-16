bc954787f0d25d20bd618cddb7279617de5c130f
Ritesh Navadiya <riteshnavadiya111@gmail.com>
2023-01-16 10:30:04 +0530

    .

--- module3/tempCodeRunnerFile.c
+++
@@ -1,6 +0,0 @@
-  // printf (" Select operation (+, -, *, /) in Calculator :-  ");  
-    // scanf ("%c", &opt); 
-    // printf (" \n Enter this first number: ");  
-    // scanf(" %d", &n1);  
-    // printf (" Enter this second number: ");  
-    // scanf (" %d", &n2); 
