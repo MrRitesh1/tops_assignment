60d2b3dc9cc1477fc34a38e3385c143357cd963f
Ritesh Navadiya <riteshnavadiya111@gmail.com>
2023-07-01 08:23:32 +0530

    module-2 qs

---
+++ react_native/Module - 2/2).WhatIsReact.createClass.js
@@ -0,0 +1,17 @@
+class Car extends React.Component {
+  render() {
+    return <h2>Hi, I am a Car!</h2>;
+  }
+}
+// ES6
+const Welcome = React.createClass({
+  render() {
+    return <h1>Welcome World!</h1>;
+  },
+});
+
+const Hello = React.createClass({
+  render: function () {
+    return <h1>Hello World!</h1>;
+  },
+});
